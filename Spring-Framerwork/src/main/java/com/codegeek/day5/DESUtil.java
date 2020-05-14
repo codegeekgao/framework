afafa5b454b50fa4d49379942e7b1f369f84fc2e
codegeekgao <codegeek_mrgao@163.com>
2020-05-14 09:39:39 +0800

    :sparkles: BeanPostProcessor接口实现+ PropertyPlaceholderConfigurer 对数据库连接信息进行解密

---
+++ Spring-Framerwork/src/main/java/com/codegeek/day5/DESUtil.java
@@ -0,0 +1,59 @@
+package com.codegeek.day5;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import java.security.Key;
+import java.security.SecureRandom;
+import java.util.Base64;
+
+public class DESUtil {
+    private static Key key;
+    private static String KEY_STR = "myKey";
+    private static String CHAR_SET_NAME = "UTF-8";
+    private static String ALGORITHM = "DES";
+
+    static {
+        try {
+            KeyGenerator generator = KeyGenerator.getInstance(ALGORITHM);
+            SecureRandom secureRandom = SecureRandom.getInstance("SHA1PRNG");
+            secureRandom.setSeed(KEY_STR.getBytes());
+            generator.init(secureRandom);
+            key = generator.generateKey();
+            generator = null;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String getEncryptString(String str) {
+        Base64.Encoder encoder = Base64.getEncoder();
+        try {
+            byte[] bytes = str.getBytes(CHAR_SET_NAME);
+            Cipher cipher = Cipher.getInstance(ALGORITHM);
+            cipher.init(Cipher.ENCRYPT_MODE, key);
+            byte[] doFinal = cipher.doFinal(bytes);
+            return encoder.encodeToString(doFinal);
+        } catch (Exception e) {
+            throw new RuntimeException();
+        }
+    }
+
+    public static String getDecryptString(String str) {
+        Base64.Decoder decoder = Base64.getDecoder();
+        try {
+            byte[] bytes = decoder.decode(str);
+            Cipher cipher = Cipher.getInstance(ALGORITHM);
+            cipher.init(Cipher.DECRYPT_MODE, key);
+            byte[] doFinal = cipher.doFinal(bytes);
+            return new String(doFinal, CHAR_SET_NAME);
+        } catch (Exception e) {
+            throw new RuntimeException();
+        }
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(getEncryptString("123456"));
+        System.out.println(getEncryptString("root"));
+    }
+}
---
+++ Spring-Framerwork/src/main/java/com/codegeek/day5/Order.java
@@ -0,0 +1,26 @@
+package com.codegeek.day5;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Order {
+
+    private String orderName;
+
+    private Double price;
+
+
+    public Order() {
+        System.out.println("order被创建了");
+    }
+
+    public void  init() {
+        System.out.println("order的init方法");
+    }
+
+    public void destroy() {
+        System.out.println("order的destroy方法");
+    }
+}
